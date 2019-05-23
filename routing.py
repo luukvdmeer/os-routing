44e835643983f3c7540f850e31c81ec0a655df8d
luukvdmeer <luukvandermeer@live.nl>
2019-05-23 10:48:44 +0200

    Initial commit

---
+++ routing.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import requests
+import json
+
+def read_coords(file, column_x, column_y, **kwargs):
+
+	csv_file = pd.read_csv(file, **kwargs)
+	coords = csv_file[[column_x, column_y]].drop_duplicates()
+	coords.columns = ['x', 'y']
+
+	return(coords)
+
+
+def get_route(event, stops, engine, geometry_encoded = True):
+
+	# Set URL composers:
+	if engine in ['graphhopper', 'gh']:
+
+		separator = '%2C'
+
+		url_start = 'http://localhost:8989/route?point=' + str(event['x']) + separator + str(event['y'])
+
+		url_points = ''
+		for index, row in stops.iterrows():
+			url_points = url_points + '&point=' + str(row['x']) + separator + str(row['y'])
+
+		url_end = '&vehicle=car'
+		if not geometry_encoded:
+			url_end = url_end + '&points_encoded=false'
+
+	elif engine in ['osrm', 'OSRM']:
+
+		url_start = 'http://127.0.0.1:5000/route/v1/driving/' + str(event['y']) + ',' + str(event['x'])
+
+		url_points = ''
+		for index, row in stops.iterrows():
+			url_points = url_points + ';' + str(row['y']) + ',' + str(row['x'])
+
+		url_end = '?steps=true&overview=full'
+		if not geometry_encoded:
+			url_end = url_end + '&geometries=geojson'
+
+	# Compose URL
+	url_full = url_start + url_points + url_end
+
+	# Request
+	response = requests.get(url_full)
+
+	# Load as json
+	route = response.json()
+
+	return(route)
---
+++ test_osrm.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import routing
+import json
+
+# Event coordinates
+event = pd.Series([48.3151658, 13.8922251], index = ['x', 'y'])
+
+# Stops coordinates
+stops = routing.read_coords(file = 'data/stops.csv', column_x = 'stop x', column_y = 'stop y', sep = ';', decimal = ',')
+
+# Get route
+route = routing.get_route(event = event, stops = stops, engine = 'OSRM')
+
+# Save on disk
+with open('outputs/route_osrm_encoded.json', 'w') as json_file:
+	json.dump(route, json_file)
+
+print(route)
+
+# Get route without encoded geometry
+route = routing.get_route(event = event, stops = stops, engine = 'OSRM', geometry_encoded = False)
+
+# Save on disk
+with open('outputs/route_osrm.json', 'w') as json_file:
+	json.dump(route, json_file)
+
+print(route)
